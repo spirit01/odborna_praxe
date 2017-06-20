b7e11009c0523deb15a3c96b188af8023cd1d884
spirit01 <petra.hrozkova@gmail.com>
2017-06-20 16:35:03 +0200

    pred sorted
    funguje basis, upraveno hledání podle znaků a ne podle indexů. list_of_all_basis hledán podle point.Basis

--- script_analysis.py
+++ script_analysis.py
@@ -14,7 +14,7 @@ from itertools import islice
 
 class Point:
    def __init__(self, Geom1 = -10, E_tot = 10000, E_scf = 10000, E_corr = 5,
-                Basis="", AuxBasis="", TCutPNO=3.3e-7, TCutPairs=1.0e-4):
+                Basis="", AuxBasis="", TCutPNO=3.3e-7, TCutPairs=1.0e-4, Method = "cat"):
          self.Geom1 = Geom1
          self.E_tot = E_tot
          self.E_scf = E_scf
@@ -23,6 +23,7 @@ class Point:
          self.AuxBasis = AuxBasis
          self.TCutPNO = TCutPNO
          self.TCutPairs = TCutPairs
+         self.Method = Method
 
    def CalcE_corr(self):
        self.E_corr = self.E_tot - self.E_scf
@@ -43,9 +44,9 @@ def get_argument():
     parser = ArgumentParser()
     parser.add_argument("-d", "--dir", dest="myDirVariable",
                         help="Choose dir", metavar="DIR", required=True)
+    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
 
     args = parser.parse_args()
-
     return(args)
 
 def find_clean_epsilon_and_twist(file):
@@ -76,7 +77,7 @@ def find_basis(i,points):
     with open(i,encoding='iso8859-1') as f:
         for line in f:
             if 'Your calculation utilizes' in line:
-                bases = line[line.index(":")+1:line.index("\n")]
+                bases = line[line.index(":")+2:line.index("\n")]
     for point in points:
         point.Basis = str(bases)
     return(points)
@@ -124,38 +125,55 @@ def find_auxbasis(i,points):
     auxbases = []
     with open(i,encoding='iso8859-1') as f:
         for line in f:
-            if '| 19> Auxbasis "' in line:
-                auxbases = line[16:len(line)-2]
+            if 'Auxbasis "' in line:
+                auxbases = line[line.index('"')+1:len(line)-2]
+                print(auxbases)
     for point in points:
         point.AuxBasis = auxbases
 
     return(points)
-"""
-def filter_by_basis(i, points):
 
-    list_of_all_basis = []
-    list_of_filtred_basis_631g = []
-    if re.search('.6-31g.', i):
-        list_of_filtred_basis.append(i)
-        print(list_of_filtred_basis)
 
-    return(points,list_of_filtred_basis)
-"""
-def make_list_of_basis(i,points):
-    a = []
-    a = (i[7:i.index("_pno")])
+def filter_by_basis(i, points,list_of_all_basis):
+    #print("HERE")
+    for point in points:
+        if point.Basis == '6-31G':
+            print("basis")
+        else:
+            print(" ")
+            #print(point.Basis)
+
+
+
+def make_list_of_basis(i,points,list_of_all_basis):
+    for point in points:
+        a = point.Basis#(i[7:i.index("_pno")])
+        if a in list_of_all_basis:
+            print("")
+        else:
+            list_of_all_basis.append(a)
+    print(list_of_all_basis)
+    return(list_of_all_basis)
 
 
 def print_points(points):
     for point in points:
         point.Print_energy()
         point.Print_parametrs_of_calculation()
-
+"""
+def find_method(i, points):
+    method = []
+    with open(i,encoding='iso8859-1') as f:
+        for line in f:
+            if 'mrcctype' in line:
+                method = line[1:len(line)-2]
+                #print(method)
+    for point in points:
+        point.Method = method
+"""
 if __name__ == '__main__':
-
     args = get_argument()
     list_of_file = []
-
     files = listdir(args.myDirVariable)
     for line in files:
         line = line.rstrip()
@@ -164,10 +182,9 @@ if __name__ == '__main__':
 
     number_of_file = len(list_of_file)
     print("Number of file for analysis:", number_of_file)
-
+    list_of_all_basis = []
     points_all = []
     for file in list_of_file:
-
         points = find_clean_epsilon_and_twist(file)
         points = find_auxbasis(file, points)
         points = find_basis(file, points)
@@ -175,7 +192,12 @@ if __name__ == '__main__':
         points = find_cutpno(file,points)
         points = find_SCF_energy(file, points)
         points = calculate_corr_E(file, points)
-        #filter_by_basis(file, points)
         points_all += points
-        make_list_of_basis(file,points)
-        print_points(points)
+        make_list_of_basis(file,points,list_of_all_basis)
+        #filter_by_basis(file, points,list_of_all_basis)
+        #print(list_of_all_basis)
+        #points = find_method(file, points)
+        if args.verbose:
+            print("")#("Verbose mode")
+        else:
+            print_points(points)
